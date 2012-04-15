95208b35fa54d236d460f3a94c046e2a5ce37b70
Alexander Dolidze <alexander.dolidze@gmail.com>
2012-04-15 17:33:30 +0400

    cleanup

---
+++ src/ge/tsu/swing/model/CalculationData.java
@@ -0,0 +1,27 @@
+package ge.tsu.swing.model;
+
+import javax.swing.JButton;
+
+public class CalculationData {
+	private static CalculationData cData = new CalculationData();
+	
+	public double mem = 0;
+	public double lastInput = 0;
+	public JButton lastActionButton;
+	public JButton lastButton;
+	public boolean dot;
+	
+	public static void reset() {
+		cData = new CalculationData();
+		cData.mem = 0;
+		cData.lastInput= 0;
+		cData.lastActionButton = null;
+		cData.lastButton = null;
+		cData.dot = false;
+	}
+	
+	public static CalculationData get() {
+		return cData;
+	}
+	
+}
